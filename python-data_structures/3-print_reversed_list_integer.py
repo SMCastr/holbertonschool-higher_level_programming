a3327c9107387d4780655072a6e7c2d3318fb47b
SMCastr <mariesweetycastro@gmail.com>
2023-10-05 14:22:47 -0400

    fix

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    for i in reversed(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+def divisible_by_2(my_list=[]):
+    """Finds all multiples of 2 in a list and returns True or False for each."""
+    result = []  # Initialize an empty list to store True/False values
+    for num in my_list:
+        if num % 2 == 0:
+            result.append(True)  # If the number is a multiple of 2, append True
+        else:
+            result.append(False)  # Otherwise, append False
+    return result  # Return the list of True/False values
