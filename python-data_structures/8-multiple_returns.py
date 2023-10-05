9fec4a042ffaf9a21e2c5e3fe9a7e9dbc794192a
SMCastr <mariesweetycastro@gmail.com>
2023-10-05 13:59:56 -0400

    fix

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!usr/bin/pythone3
-def multiple_returns(sentence):
-    """Devuelve una tupla con la longitud de una cadena y su primer carácter."""
-    if not sentence:
-        return (0, None)  # Si la cadena está vacía, devuelve (0, None)
+def multiple_returns(sentence):    
+    if len(sentence) == 0:
+        return (0, None)
     else:
         return (len(sentence), sentence[0])
