c4b4adf2045c4957b2d463b5dc1b6bc9a1486325
SMCastr <mariesweetycastro@gmail.com>
2023-10-06 13:00:04 -0400

    fix & modify

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!usr/bin/pythone3
-def multiple_returns(sentence):    
-    if len(sentence) == 0:
+def multiple_returns(sentence):
+    if not sentence:
         return (0, None)
     else:
         return (len(sentence), sentence[0])
