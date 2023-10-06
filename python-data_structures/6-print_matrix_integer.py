550ba942252bb33ed03ee5a4cebf4e5af4f0fc63
SMCastr <mariesweetycastro@gmail.com>
2023-10-06 13:03:01 -0400

    fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, num in enumerate(row):
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
             if i < len(row) - 1:
-                print("{:d}".format(num), end=" ")
-            else:
-                print("{:d}".format(num))
+                print(" ", end="")
+        print()               
+        print("{:d}".format(num))
+        if num % 2 == 0:
+            result.append(True)
+        else:
+            result.append(False)
+    return result
