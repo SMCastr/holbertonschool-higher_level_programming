02ea3835c501174a7a35843374a57b60c97ad906
SMCastr <mariesweetycastro@gmail.com>
2023-10-06 16:11:51 -0400

    fix

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
 def raise_exception():
     try:
         raise TypeError("An exception was raised")
     except TypeError as te:
         print("Exception raised")
         raise te
-    
