680b7b94b9609044579ec85fd42666498bc6bee3
SMCastr <mariesweetycastro@gmail.com>
2023-10-06 19:37:01 -0400

    fix

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except ValueError:
-        return False
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return (None)
+    elif idx > len(my_list) - 1:
+        return (None)
     else:
-        return True
-    
+        return (my_list[idx])
