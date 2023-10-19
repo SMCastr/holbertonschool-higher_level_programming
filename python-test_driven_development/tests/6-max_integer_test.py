7c9a305e672c477d7f230070123a89339b01193a
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 16:13:23 -0400

    Modify functions

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -3,7 +3,7 @@
 """
 
 import unittest
-from max_integer import max_integer
+from 6-max_integer import max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
@@ -48,6 +48,30 @@ class TestMaxInteger(unittest.TestCase):
         # Test with a list of mixed data types
         with self.assertRaises(TypeError):
             max_integer([1, "2", 3, "4"])
+
+    def test_max_at_the_end(self):
+        # Test with the maximum at the end of the list
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+
+    def test_max_at_the_beginning(self):
+        # Test with the maximum at the beginning of the list
+        self.assertEqual(max_integer([5, 1, 2, 3, 4]), 5)
+
+    def test_max_in_the_middle(self):
+        # Test with the maximum in the middle of the list
+        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
+
+    def test_one_negative_number(self):
+        # Test with only one negative number in the list
+        self.assertEqual(max_integer([-1, 2, 3, 4]), 4)
+
+    def test_only_negative_numbers(self):
+        # Test with only negative numbers in the list
+        self.assertEqual(max_integer([-5, -3, -9, -1]), -1)
+
+    def test_list_of_one_element(self):
+        # Test with a list containing one element
+        self.assertEqual(max_integer([42]), 42)
 
 if __name__ == '__main__':
     unittest.main()
