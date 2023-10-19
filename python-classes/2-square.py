80cd627fa29a1a8c8205f96923220a0258eedc0b
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 11:12:23 -0400

    fix and add module

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,47 @@
 #!/usr/bin/python3
+"""
+This module defines the Square class with size validation.
+"""
+
+
 class Square:
+    """
+    The Square class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
     def __init__(self, size=0):
-        if type(size) is not int:
+        """
+        Initializes an instance of the Square class with a given size.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print(type(my_square_1))
+    print(my_square_1.__dict__)
+
+    my_square_2 = Square()
+    print(type(my_square_2))
+    print(my_square_2.__dict())
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size
