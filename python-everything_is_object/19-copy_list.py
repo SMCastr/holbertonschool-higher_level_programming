84fb9ca3391f79d93a9675de8a4ee67e85ee99ce
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 10:33:41 -0400

    fix and add code

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -2,3 +2,19 @@
 
 def copy_list(a_list):
     return a_list.copy()
+
+# Test cases
+test_cases = [
+    [1, 2, 3],
+    [1],
+    [1, "2", 3],
+    [1, [2, 3], {'id': 12}],
+    []
+]
+
+for my_list in test_cases:
+    print(f"Original List: {my_list}")
+    new_list = copy_list(my_list)
+    print(f"Copied List: {new_list}")
+    print(f"Is Copied List Equal: {new_list == my_list}")
+    print(f"Is Copied List Identical: {new_list is my_list}")
