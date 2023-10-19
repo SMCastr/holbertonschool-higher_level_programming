610377d29e972b0c0b46507c8c2f3b4f9a3d620f
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 10:23:29 -0400

    fix code

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -2,14 +2,3 @@
 
 def copy_list(a_list):
     return a_list.copy()
-
-my_list = [1, 2, 3]
-print(my_list)
-
-new_list = copy_list(my_list)
-
-print(my_list)
-print(new_list)
-
-print(new_list == my_list)
-print(new_list is my_list)
