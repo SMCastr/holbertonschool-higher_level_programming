fa341bcc6deabe803bf15538354c51b514ed90e5
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 16:52:05 -0400

    modify

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""This module defines a class Rectangle that represents a rectangle."""
+"""A class Rectangle that defines a rectangle."""
 
 
 class Rectangle:
     """Define a Rectangle with width and height attributes."""
-
+  
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle with optional width and height."""
         self.width = width
@@ -43,22 +43,19 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        """Calculate and return the perimeter of the
-        rectangle."""
+        """Calculate and return the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return a string representation of the
-        rectangle using # characters."""
+        """Return a string representation of the rectangle using # characters."""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(['#' * self.__width] * self.__height)
 
     def __repr__(self):
-        """Return a string representation of the
-        rectangle that can recreate the object."""
+        """Return a string representation of the rectangle that can recreate the object."""
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
 
