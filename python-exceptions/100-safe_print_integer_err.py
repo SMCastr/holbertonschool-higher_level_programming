a095f734eb30d5ca1207c495b3a13b19b8ab0320
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 12:49:10 -0400

    fix blank

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -10,11 +10,13 @@ def safe_print_integer_err(value):
         print("Exception: {}".format(e), file=sys.stderr)
         return False
 
+
 if __name__ == "__main__":
     value = 89
     has_been_print = safe_print_integer_err(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
+
 
     value = -89
     has_been_print = safe_print_integer_err(value)
