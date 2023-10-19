f63e3e4d2634a1c48c30c9a7658bf2b4fef3bb5e
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 11:15:06 -0400

    fix and add module

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -28,7 +28,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
 if __name__ == "__main__":
     my_square_1 = Square(3)
     print(type(my_square_1))
@@ -36,7 +35,7 @@ if __name__ == "__main__":
 
     my_square_2 = Square()
     print(type(my_square_2))
-    print(my_square_2.__dict())
+    print(my_square_2.__dict__)
 
     try:
         print(my_square_1.size)
@@ -44,4 +43,27 @@ if __name__ == "__main__":
         print(e)
 
     try:
-        print(my_square_1.__size
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_3 = Square("3")
+        print(type(my_square_3))
+        print(my_square_3.__dict__)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_4 = Square(3.14)
+        print(type(my_square_4))
+        print(my_square_4.__dict__)
+    except Exception as e:
+        print(e)
+
+    try:
+        my_square_5 = Square(-89)
+        print(type(my_square_5))
+        print(my_square_5.__dict__)
+    except Exception as e:
+        print(e)
