fd130284633c741b5507a3e34bc55f6242043d0c
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 13:19:55 -0400

    fix and add module and functions code

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 class Node:
+    """
+    Class Node defines a node of a singly linked list.
+
+    Attributes:
+        data (int): The data stored in the node.
+        next_node (Node): The reference to the next node in the list.
+    """
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
@@ -26,10 +33,19 @@ class Node:
         self.__next_node = value
 
 class SinglyLinkedList:
+    """
+    Class SinglyLinkedList defines a singly linked list.
+
+    Attributes:
+        head: The head of the linked list.
+    """
     def __init__(self):
         self.head = None
 
     def sorted_insert(self, value):
+        """
+        Inserts a new Node into the correct sorted position in the list (increasing order).
+        """
         new_node = Node(value)
         if self.head is None or value < self.head.data:
             new_node.next_node = self.head
@@ -51,16 +67,55 @@ class SinglyLinkedList:
 
 # Test the SinglyLinkedList class
 if __name__ == "__main__":
+    # Additional test cases
+    n1 = Node(3)
+    n2 = Node(-5)
+    n3 = Node(4)
+    n3.next_node = n2
+
+    try:
+        n4 = Node("4")
+    except Exception as e:
+        print(e)
+
+    try:
+        n2.next_node = "Node"
+    except Exception as e:
+        print(e)
+
     sll = SinglyLinkedList()
     sll.sorted_insert(2)
-    sll.sorted_insert(5)
-    sll.sorted_insert(3)
-    sll.sorted_insert(10)
+    print(sll)
+
+    sll = SinglyLinkedList()
     sll.sorted_insert(1)
-    sll.sorted_insert(-4)
-    sll.sorted_insert(-3)
-    sll.sorted_insert(4)
-    sll.sorted_insert(5)
-    sll.sorted_insert(12)
+    sll.sorted_insert(2)
     sll.sorted_insert(3)
+    sll.sorted_insert(4)
+    print(sll)
+
+    sll = SinglyLinkedList()
+    sll.sorted_insert(1)
+    sll.sorted_insert(2)
+    sll.sorted_insert(3)
+    sll.sorted_insert(4)
+    sll.sorted_insert(4)
+    sll.sorted_insert(3)
+    sll.sorted_insert(2)
+    sll.sorted_insert(1)
+    print(sll)
+
+    sll = SinglyLinkedList()
+    sll.sorted_insert(10)
+    sll.sorted_insert(2)
+    sll.sorted_insert(-3)
+    sll.sorted_insert(34)
+    sll.sorted_insert(4)
+    sll.sorted_insert(-5)
+    sll.sorted_insert(0)
+    sll.sorted_insert(8)
+    sll.sorted_insert(7)
+    print(sll)
+
+    sll = SinglyLinkedList()
     print(sll)
