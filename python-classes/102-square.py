6cb00bdf775330eb78b9c52a840efe9d4d622868
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 14:26:41 -0400

    create file, add code functions

---
+++ python-classes/102-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Class Square defines a square and provides comparison methods.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter for the size attribute.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter for the size attribute.
+
+        Args:
+            value (int): The size to be set.
+
+        Raises:
+            TypeError: If size is not a number.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Computes and returns the area of the square.
+
+        Returns:
+            float: The area of the square.
+        """
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
