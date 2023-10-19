6527f647b99938f2527cb16f189417366a270f4d
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 15:39:07 -0400

    fix and add blank line.

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,13 +9,16 @@ def matrix_divided(matrix, div):
         div (int or float): The number to divide the matrix by.
 
     Returns:
-        list of lists: A new matrix with all elements divided by div, rounded to 2 decimal places.
+        list of lists: A new matrix with all elements divided by div,
+        rounded to 2 decimal places.
 
     Raises:
         TypeError: If matrix is not a list of lists of integers or floats,
-                   if each row of the matrix is not of the same size, or if div is not a number.
+                   if each row of the matrix is not of the same size,
+                   or if div is not a number.
         ZeroDivisionError: If div is equal to 0.
     """
+    
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
