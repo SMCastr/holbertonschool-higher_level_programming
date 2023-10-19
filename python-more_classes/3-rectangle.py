0c2e03fa802092300e3bba8bf1c4e4169be2876c
SMCastr <mariesweetycastro@gmail.com>
2023-10-19 16:46:50 -0400

    modify

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """Define a Rectangle with width and height attributes."""
- 
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle with optional width and height."""
         self.width = width
@@ -49,11 +49,13 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return a string representation of the rectangle using # characters."""
+        """Return a string representation of the
+        rectangle using # characters."""
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(['#' * self.__width] * self.__height)
 
     def __repr__(self):
-        """Return a string representation of the rectangle that can recreate the object."""
+        """Return a string representation of the
+        rectangle that can recreate the object."""
         return "Rectangle({}, {})".format(self.__width, self.__height)
