89b931d1c7a63ebf314c0a1ec011c58f802746f7
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 16:19:22 -0400

    Modify code functions

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+""" Module for Student class
+"""
 
 class Student:
     """
-    A class that defines a student with attributes first_name, last_name, and age.
-    """
+    Defines a student.
 
+    Attributes:
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
+    """
 
     def __init__(self, first_name, last_name, age):
         """
-        Initialize a Student instance with first_name, last_name, and age.
+        Initializes a new Student.
 
         Args:
             first_name (str): The first name of the student.
@@ -19,15 +25,23 @@ class Student:
         self.last_name = last_name
         self.age = age
 
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        return self.__dict__
 
 # Sample usage for testing
-
 if __name__ == "__main__":
-    student_1 = Student("Tom", "Smith", 89)
-    print(student_1)  # Correct output - Student(“Tom”, “Smith”, 89)
+    students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
 
-    student_2 = Student("I have a long name", "A very long name, not you?", -89)
-    print(student_2)  # Correct output - Student(“I have a long name”, “A very long name, not you?”, -89)
-
-    student_3 = Student("", "", 0)
-    print(student_3)  # Correct output - Student("", "", 0)
+    for student in students:
+        j_student = student.to_json()
+        print(type(j_student))
+        print(j_student['first_name'])
+        print(type(j_student['first_name']))
+        print(j_student['age'])
+        print(type(j_student['age']))
