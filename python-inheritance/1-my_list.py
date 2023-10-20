ead4cd08311507ac4839fce0a5ab0e33b074c274
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 14:29:26 -0400

    fix and add code functions

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
+"""
+Module for MyList class.
+"""
+
 
 class MyList(list):
-    def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
+    """
+    MyList class that inherits from list.
+    """
 
-if __name__ == '__main__':
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
+    def print_sorted(self):
+        """
+        Print the list in ascending order.
+        """
+        print(sorted(self))
