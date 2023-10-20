4987a8d650f2a79802c8ec9c79a4d27286956c53
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 15:49:17 -0400

    fix and warnings

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python3
-
-"""This module defines a class Student."""
 class Student:
-    """A class that represents a student with attributes first_name, last_name, and age."""
+    """
+    A class that defines a student with attributes first_name, last_name, and age.
+    """
 
     def __init__(self, first_name, last_name, age):
+        """
+        Initialize a Student instance with first_name, last_name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Convert the student object to a JSON-compatible dictionary with optional attribute filter."""
-        if attrs is None:
-            return self.__dict__
-        return {attr: getattr(self, attr) for attr in attrs}
+    def to_json(self):
+        """
+        Convert the student object to a JSON-compatible dictionary.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        return {key: getattr(self, key) for key in self.__dict__.keys()}
+
+# Sample usage for testing
+if __name__ == "__main__":
+    student_1 = Student("John", "Snow", 25)
+    print(student_1.to_json())  # Correct output - to_json()
+    
+    print(student_1.to_json(['first_name']))  # Correct output - to_json(['first_name'])
+    
+    print(student_1.to_json(['first_name', 'age']))  # Correct output - to_json(['first_name', 'age'])
+    
+    print(student_1.to_json([]))  # Correct output - to_json([])
+    
+    print(student_1.to_json(['first_name', 'saymyname', 'last_name', 'Age']))  # Correct output - to_json(['first_name', 'saymyname', 'last_name', 'Age'])
