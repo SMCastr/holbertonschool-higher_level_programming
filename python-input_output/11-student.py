8b2e93f267a0846b2a05bf53a9d4fe51aa0cda3b
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 16:36:18 -0400

    modify

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -3,15 +3,8 @@
 This module defines a Student class that has methods for JSON serialization and deserialization.
 """
 
+
 class Student:
-    """
-    Defines a student with first_name, last_name, and age attributes.
-
-    Methods:
-        to_json(self, attrs=None): Retrieves a dictionary representation of a Student instance.
-        reload_from_json(self, json): Replaces all attributes of the Student instance from a JSON dictionary.
-    """
-
     def __init__(self, first_name, last_name, age):
         """
         Initializes a new Student.
@@ -48,39 +41,3 @@ class Student:
         """
         for key, value in json.items():
             setattr(self, key, value)
-
-# Sample usage for testing
-if __name__ == "__main__":
-    import os
-    import sys
-
-    path = sys.argv[1]
-
-    if os.path.exists(path):
-        os.remove(path)
-
-    student_1 = Student("John", "Doe", 23)
-    j_student_1 = student_1.to_json()
-    print("Initial student:")
-    print(student_1)
-    print(type(student_1))
-    print(type(j_student_1))
-    print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
-
-    save_to_json_file(j_student_1, path)
-    read_file(path)
-    print("\nSaved to disk")
-
-    print("Fake student:")
-    new_student_1 = Student("Fake", "Fake", 89)
-    print(new_student_1)
-    print(type(new_student_1))
-    print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
-
-    print("Load dictionary from file:")
-    new_j_student_1 = load_from_json_file(path)
-
-    new_student_1.reload_from_json(j_student_1)
-    print(new_student_1)
-    print(type(new_student_1))
-    print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
