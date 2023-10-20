75c6fd890df30e74d2ef7b0eb6def2dd328e8ec0
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 16:27:00 -0400

    fix f blanks

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -13,7 +13,6 @@ class Student:
         age (int): The age of the student.
     """
 
-
     def __init__(self, first_name, last_name, age):
         """
         Initializes a new Student.
@@ -26,7 +25,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
 
     def to_json(self):
         """
