20de8cc4a18859d3d94cce9715edd176a4b52d79
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 14:11:22 -0400

    add code functions

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    This function takes an object 'obj' as input and returns a list of its available attributes and methods.
+    
+    Args:
+        obj: The object for which to retrieve attributes and methods.
+
+    Returns:
+        A list of attribute and method names as strings.
+
+    """
+    return dir(obj)
+
+# Example usage
+if __name__ == "__main__":
+    class MyClass1(object):
+        pass
+
+    class MyClass2(object):
+        my_attr1 = 3
+
+        def my_meth(self):
+            pass
+
+    print(lookup(MyClass1))
+    print(lookup(MyClass2))
+    print(lookup(int))
