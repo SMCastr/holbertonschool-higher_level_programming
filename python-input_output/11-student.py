e7fb9ee9082cafc04741fae4c5badbd5ea2c2407
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 15:42:12 -0400

    fix and add functions code

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-
 class Student:
-    """A class that represents a student with attributes first_name, last_name, and age."""
+    """
+    A class that represents a student with attributes first_name, last_name, and age.
+    """
+
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a Student instance with first_name, last_name, and age."""
+        """
+        Initialize a Student instance with first_name, last_name, and age.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -30,9 +39,6 @@ class Student:
 
         Args:
             json (dict): A dictionary with attribute-value pairs.
-
-        Returns:
-            None
         """
         for key, value in json.items():
             setattr(self, key, value)
