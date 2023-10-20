fa186cc225aba33a1df403bf691118303b1c3ed1
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 11:01:44 -0400

    modify

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -71,15 +71,13 @@ class Rectangle:
             return 0
         return 2 * (self.__width + self.__height)
 
-  def __str__(self):
-    """
-    Return a string representation of the rectangle using print_symbol.
-    """
-    if self.__width == 0 or self.__height == 0:
-        return ""
-    return "\n".join(
-        [str(self.print_symbol) * self.__width] * self.__height
-    )
+    def __str__(self):
+        """
+        Return a string representation of the rectangle using print_symbol.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join([str(self.print_symbol) * self.__width] * self.__height)
 
     def __repr__(self):
         """
