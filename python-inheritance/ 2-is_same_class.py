e378a6befb6cde02de82ce6f4507a252bc5cb2e6
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 23:27:01 -0400

    add files and code functions

--- python-inheritance/ 2-is_same_class.py
+++ python-inheritance/ 2-is_same_class.py
@@ -1,14 +1,5 @@
-#!/usr/bin/python3
-
 def is_same_class(obj, a_class):
     """
-    Check if an object is exactly an instance of the specified class.
-
-    Args:
-        obj: The object to check.
-        a_class: The class to compare the object with.
-
-    Returns:
-        True if the object is an instance of the specified class; False otherwise.
+    Returns True if obj is exactly an instance of a_class, otherwise False.
     """
-    return type(obj) == a_class
+    return type(obj) is a_class
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,29 +1,5 @@
 #!/usr/bin/python3
 
 class MyList(list):
-    """
-    MyList is a subclass of the built-in 'list' class. It inherits all the functionality of the list class.
-
-    Public Methods:
-        print_sorted(self): Prints the list in ascending order.
-    """
-
     def print_sorted(self):
-        """
-        Print the list in ascending order.
-        """
-        sorted_list = sorted(self)
-        print(sorted_list)
-
-# Example usage
-
-if __name__ == "__main__":
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)  # Print the unsorted list
-    my_list.print_sorted()  # Print the sorted list
-    print(my_list)  # Print the original unsorted list again
+        print(sorted(self))
