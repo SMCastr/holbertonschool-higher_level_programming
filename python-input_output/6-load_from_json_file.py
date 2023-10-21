a8ce53e1ec73da68c97d861c1f41d2d6445355e3
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 22:45:50 -0400

    fix and  add code functions

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -7,8 +7,35 @@ def load_from_json_file(filename):
     Create an object from a JSON file.
 
     :param filename: The name of the JSON file to load.
-    :return: The Python object represented by the JSON data in the file.
+    :return: The Python object loaded from the JSON file.
     """
     with open(filename, 'r') as file:
-        data = json.load(file)
-        return data
+        return json.load(file)
+
+if __name__ == "__main__":
+    # Example usage and test cases
+    filename = "my_list.json"
+    my_list = load_from_json_file(filename)
+    print(my_list)
+    print(type(my_list))
+
+    filename = "my_dict.json"
+    my_dict = load_from_json_file(filename)
+    print(my_dict)
+    print(type(my_dict))
+
+    try:
+        filename = "my_set_doesnt_exist.json"
+        my_set = load_from_json_file(filename)
+        print(my_set)
+        print(type(my_set))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        filename = "my_fake.json"
+        my_fake = load_from_json_file(filename)
+        print(my_fake)
+        print(type(my_fake))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
