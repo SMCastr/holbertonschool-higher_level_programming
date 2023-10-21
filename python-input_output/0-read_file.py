8492825dcfb1f31704e0d1337cd5d7154f1efaa3
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 23:16:13 -0400

    add en fixcode

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    """Read a text file and print its content to stdout.
+    """
+    Reads a text file (UTF8) and prints it to stdout.
 
     Args:
-        filename (str): The name of the text file to read.
-
-    Returns:
-        None
+        filename (str): The name of the file to be read.
     """
-    with open(filename, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line, end="")
+    with open(filename, mode='r', encoding='utf-8') as file:
+        print(file.read(), end="")
