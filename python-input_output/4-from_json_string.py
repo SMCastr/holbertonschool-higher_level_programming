71cb91e5941442b1bb7ebf963418d252425a09d0
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 22:56:48 -0400

    fix and code functions

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -3,12 +3,38 @@
 import json
 
 def from_json_string(my_str):
-    """Convert a JSON string to a Python data structure.
+    """
+    Return an object represented by a JSON string.
 
     Args:
-        my_str (str): The JSON string to be converted to a Python object.
+        my_str (str): A JSON string.
 
     Returns:
-        obj: The Python data structure represented by the JSON string.
+        object: The Python data structure represented by the JSON string.
     """
     return json.loads(my_str)
+
+if __name__ == "__main__":
+    # Example usage and test cases
+    s_my_list = "[1, 2, 3]"
+    my_list = from_json_string(s_my_list)
+    print(my_list)
+    print(type(my_list))
+
+    s_my_dict = """
+    {"is_active": true, "info": {"age": 36, "average": 3.14}, 
+    "id": 12, "name": "John", "places": ["San Francisco", "Tokyo"]}
+    """
+    my_dict = from_json_string(s_my_dict)
+    print(my_dict)
+    print(type(my_dict))
+
+    try:
+        s_my_dict = """
+        {"is_active": true, 12 }
+        """
+        my_dict = from_json_string(s_my_dict)
+        print(my_dict)
+        print(type(my_dict))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
