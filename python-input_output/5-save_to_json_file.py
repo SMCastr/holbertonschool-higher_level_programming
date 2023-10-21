42335554c49ad798850bb7ce56180557a27d9bd9
SMCastr <mariesweetycastro@gmail.com>
2023-10-20 22:54:14 -0400

    fix blank-line

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,6 +5,7 @@ This module defines a function to save an object to a text file in JSON format.
 
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """
     Save an object to a text file using JSON representation.
