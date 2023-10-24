4a61e5b46d3232bdd73a8a7b8a3c1bbb2ccaf977
SMCastr <mariesweetycastro@gmail.com>
2023-10-24 15:08:05 -0400

    fix module

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 """ Module that creates a locked class"""
 class LockedClass:
+    """ Class that prevents the user from
+    dynamically creating new instance"""
     __slots__ = ['first_name']
