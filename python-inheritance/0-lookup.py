2a05f6ef257ce4d688c1ce2c84de628167eccbbd
SMCastr <mariesweetycastro@gmail.com>
2023-10-24 19:00:58 -0400

    readme fix

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
+"""Module for BaseGeometry class"""
+
 
 def lookup(obj):
-    """
-    Return the list of available attributes and methods of an object.
+    """Returns the list of available attributes and methods of an object"""
 
-    Args:
-        obj: The object for which attributes and methods should be looked up.
-
-    Returns:
-        A list of strings representing attributes and methods of the object.
-    """
     return dir(obj)
