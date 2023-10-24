a81b0f1ba275eeb98696086ca0b6fdbabcb75b88
SMCastr <mariesweetycastro@gmail.com>
2023-10-24 19:48:42 -0400

    space

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,6 +5,7 @@ that returns True if the object is exactly"""
 
 def is_same_class(obj, a_class):
     """
-    Check if an object is exactly an instance of the specified class
+    Check if an object is exactly an instance of the
+    specified class
     """
     return type(obj) == a_class
