f280bc7797a52530b04aa65683ba1691bf9de40a
SMCastr <mariesweetycastro@gmail.com>
2023-10-24 19:04:53 -0400

    fix code functionslist

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
+    """Returns the list of available attributes 
+    and methods of an object"""
 
     return dir(obj)
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
 class MyList(list):
+    """ Class that inherits from list. """
     def print_sorted(self):
         """
         Print the list sorted in ascending order.
-
-        Attributes:
-            self: The object itself.
-
-        Returns:
-            None.
         """
-        print(sorted(self))
+        sorted = []
+        for i in self:
+            sorted.append(i)
+        sorted.sort()
+        print(sorted)
