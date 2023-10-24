a0d5132351bda9c3e868ba512390df70524463ff
SMCastr <mariesweetycastro@gmail.com>
2023-10-24 19:56:03 -0400

    fix module

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
+""" Module that contains a function."""
+
 
 def inherits_from(obj, a_class):
     """
-    Check if an object is an instance of a class that inherited from the specified class.
-
-    Args:
-        obj: The object to check.
-        a_class: The class to compare the object with.
-
-    Returns:
-        True if the object is an instance of a class that inherited from the specified class; False otherwise.
+    Check if an object is an instance of a
+    class that inherited from the specified class.
     """
     return issubclass(type(obj), a_class) and type(obj) != a_class
