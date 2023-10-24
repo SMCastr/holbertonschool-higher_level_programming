2d182e58205e7e78729b4b6d76fd294ba878116a
SMCastr <mariesweetycastro@gmail.com>
2023-10-24 13:49:33 -0400

    102

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -1,66 +1,58 @@
 #!/usr/bin/python3
+"""This module contains a class that defines a square"""
 
 class Square:
-    """
-    Class Square defines a square and provides comparison methods.
-
-    Attributes:
-        __size (int): The size of the square.
-    """
-    def __init__(self, size=0):
+    """this class defines a square"""
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """
-        Getter for the size attribute.
-
-        Returns:
-            int: The size of the square.
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Setter for the size attribute.
-
-        Args:
-            value (int): The size to be set.
-
-        Raises:
-            TypeError: If size is not a number.
-            ValueError: If size is less than 0.
-        """
-        if not isinstance(value, (int, float)):
-            raise TypeError("size must be a number")
-        if value < 0:
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
-        """
-        Computes and returns the area of the square.
-
-        Returns:
-            float: The area of the square.
-        """
         return self.__size ** 2
 
-    def __eq__(self, other):
-        return self.area() == other.area()
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
 
-    def __ne__(self, other):
-        return self.area() != other.area()
+    def __str__(self):
+        if self.__size == 0:
+            return ""
+        else:
+            res = ""
+            for i in range(self.__position[1]):
+                res += "\n"
+            for i in range(self.__size):
+                res += " " * self.__position[0] + "#" * self.__size
+                if i != self.__size - 1:
+                    res += "\n"
+            return res
 
-    def __lt__(self, other):
-        return self.area() < other.area()
-
-    def __le__(self, other):
-        return self.area() <= other.area()
-
-    def __gt__(self, other):
-        return self.area() > other.area()
-
-    def __ge__(self, other):
-        return self.area() >= other.area()
