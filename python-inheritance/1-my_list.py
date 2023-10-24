a19dbbd70ba553d05ca1a256d76df82aedfdddaf
SMCastr <mariesweetycastro@gmail.com>
2023-10-24 19:37:58 -0400

    fix module

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+"""This module contains class inherit from list."""
 
 
 class MyList(list):
-    """ Class that inherits from list. """
+    """Class MyList that inherits from list"""
     def print_sorted(self):
-        """
-        Print the list sorted in ascending order.
-        """
+        """Prints the list, but sorted (ascending sort)"""
         sorted = []
         for i in self:
             sorted.append(i)
