af40edb053b4e6891cca4318bcf5d2ec6eb69c0d
SMCastr <mariesweetycastro@gmail.com>
2023-10-26 00:18:31 -0400

    fix module commtn

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,15 +3,17 @@
 
 
 import sys
-import os
+import os.path
 
-if os.path.exists("add_item.json"):
-    from load_from_json_file import load_from_json_file
-    my_list = load_from_json_file("add_item.json")
-else:
-    my_list = []
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my_list.extend(sys.argv[1:])
+my_file = 'add_item.json'
+my_list = []
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
 
-from save_to_json_file import save_to_json_file
-save_to_json_file(my_list, "add_item.json")
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+save_to_json_file(my_list, my_file)
