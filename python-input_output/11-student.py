88dfeadacede61a75b0601709c0f602a1a6f7fe7
SMCastr <mariesweetycastro@gmail.com>
2023-10-26 11:36:35 -0400

    fix structure lines-spaces

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -8,11 +8,6 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """
         Initializes a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -21,12 +16,6 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of a Student instance.
-
-        Args:
-            attrs (list): A list of attributes to include in the dictionary (default is None).
-
-        Returns:
-            dict: A dictionary containing the student's attributes.
         """
         if attrs is None:
             return self.__dict__
@@ -34,10 +23,8 @@ class Student:
 
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance from a JSON dictionary.
-
-        Args:
-            json (dict): A dictionary containing attribute names and values.
+        Replaces all attributes of the Student instance from
+        a JSON dictionary.
         """
         for key, value in json.items():
             setattr(self, key, value)
