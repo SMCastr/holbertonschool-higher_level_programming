29ef23766a1fd8c0dc3b524faf9d15d6856f0102
SMCastr <mariesweetycastro@gmail.com>
2023-10-26 12:02:28 -0400

    fix module and functions

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """ Module for append_after"""
 
-
-append_after = __import__('100-append_after').append_after
-
-
-append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
-"""Inserts a line of text to a file after each line containing a specific string."""
-
-
 def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file after each
+    line containing a specific string.
+    """
     with open(filename, 'r') as file:
         lines = file.readlines()
 
@@ -18,3 +14,6 @@ def append_after(filename="", search_string="", new_string=""):
             file.write(line)
             if search_string in line:
                 file.write(new_string)
+
+if __name__ == "__main__":
+    append_after("append_after_100.txt", "Python", "\"C is fun!\"\n")
