2e2c1971cfe6931d0cc02c4b0f4c0c2b6c3e030f
SMCastr <mariesweetycastro@gmail.com>
2023-10-26 12:17:22 -0400

    fxi

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """
 Log parsing and metrics computation.
-
 This script reads log data from stdin, computes metrics,
 and prints statistics.
-The input log format is expected to be: 
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size>
 """
 
 import sys
+from unittest.main import MAIN_EXAMPLES
 
 def print_stats(total_size, status_codes):
     """
@@ -21,11 +18,6 @@ def print_stats(total_size, status_codes):
     for code, count in sorted_status_codes:
         if count > 0:
             print("{:s}: {:d}".format(code, count))
-
-def main():
-    total_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-    count = 0
 
     try:
         # Read log data from stdin
@@ -51,5 +43,5 @@ def main():
         raise
 
 if __name__ == "__main__":
-    main()
+    MAIN_EXAMPLES()
 
