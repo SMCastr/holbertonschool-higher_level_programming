b78e2f71f440a03d83c03ef81dccec6fbb5c1f64
SMCastr <mariesweetycastro@gmail.com>
2023-10-27 14:07:07 -0400

    blank-spaces

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -6,7 +6,7 @@ def add_attribute(obj, attr_name, attr_value):
     """
     Add a new attribute to an object if it's possible.
     """
-    
+
     if not hasattr(obj, '__dict__') and not (hasattr(obj, '__slots__')
             and attr_name in obj.__slots__):
         raise TypeError("can't add new attribute")
