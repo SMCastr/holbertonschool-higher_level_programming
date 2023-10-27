41bc0dc53d6eae5ea22a634243d1616fe1ba53dc
SMCastr <mariesweetycastro@gmail.com>
2023-10-27 15:51:38 -0400

    fixing file module structure

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-""" 0-main"""
 
 import unittest
 from models.base import Base
@@ -54,98 +53,111 @@ class TestBase(unittest.TestCase):
 
         cls.assertEqual(list_squares_input, list_squares_output)
 
-    # def test_load_from_file(cls):
+    def test_load_from_file(cls):
+        """Test the load_from_file method of the Base class"""
+        # load a list of Rectangle instances from a file
+        list_rectangles = Rectangle.load_from_file()
+        cls.assertIsInstance(list_rectangles, list)
 
-    # def test_create(self):
-    #     """Test the create method of the Base class"""
-    #     # Test creating a Rectangle object
-    #     rectangle_dict = {'id': 1, 'width': 2, 'height': 3, 'x': 4, 'y': 5}
-    #     rectangle = Base.create(**rectangle_dict)
-    #     self.assertEqual(rectangle.id, 1)
-    #     self.assertEqual(rectangle.width, 2)
-    #     self.assertEqual(rectangle.height, 3)
-    #     self.assertEqual(rectangle.x, 4)
-    #     self.assertEqual(rectangle.y, 5)
+        for rectangle in list_rectangles:
+            cls.assertIsInstance(rectangle, Rectangle)
 
-    #     # Test creating a Square object
-    #     square_dict = {'id': 2, 'size': 6, 'x': 7, 'y': 8}
-    #     square = Base.create(**square_dict)
-    #     self.assertEqual(square.id, 2)
-    #     self.assertEqual(square.size, 6)
-    #     self.assertEqual(square.x, 7)
-    #     self.assertEqual(square.y, 8)
+        # load a list of Square instances from a file
+        list_squares = Square.load_from_file()
+        cls.assertIsInstance(list_squares, list)
 
-    # def test_save_to_file(self):
-    #     """Test the save_to_file method of the Base class"""
-    #     # Test with a list of Rectangle objects
-    #     r1 = Rectangle(10, 7, 2, 8)
-    #     r2 = Rectangle(2, 4)
-    #     list_rectangles = [r1, r2]
-    #     Base.save_to_file(list_rectangles)
-    #     with open("Rectangle.json", "r") as file:
-    #         self.assertEqual(file.read(
-    #         ), '[{"y": 8, "x": 2, "id": 1, "width": 10, "height": 7}, {"y": 0, "x": 0, "id": 2, "width": 2, "height": 4}]')
+        for square in list_squares:
+            cls.assertIsInstance(square, Square)
 
-    #     # Test with a list of Square objects
-    #     s1 = Square(10, 2, 8)
-    #     s2 = Square(2)
-    #     list_squares = [s1, s2]
-    #     Base.save_to_file(list_squares)
-    #     with open("Square.json", "r") as file:
-    #         self.assertEqual(file.read(
-    #         ), '[{"id": 3, "size": 10, "x": 2, "y": 8}, {"id": 4, "size": 2, "x": 0, "y": 0}]')
+    def test_create(self):
+        """Test the create method of the Base class"""
+         # Test creating a Rectangle object
+        rectangle_dict = {'id': 1, 'width': 2, 'height': 3, 'x': 4, 'y': 5}
+        rectangle = Base.create(**rectangle_dict)
+        self.assertEqual(rectangle.id, 1)
+        self.assertEqual(rectangle.width, 2)
+        self.assertEqual(rectangle.height, 3)
+        self.assertEqual(rectangle.x, 4)
+        self.assertEqual(rectangle.y, 5)
 
-    # def test_load_from_file(self):
-    #     """Test the load_from_file method of the Base class"""
-    #     # Test with a list of Rectangle objects
-    #     list_rectangles = Base.load_from_file()
-    #     self.assertEqual(list_rectangles, [{'y': 8, 'x': 2, 'id': 1, 'width': 10, 'height': 7}, {
-    #                      'y': 0, 'x': 0, 'id': 2, 'width': 2, 'height': 4}])
+         # Test creating a Square object
+        square_dict = {'id': 2, 'size': 6, 'x': 7, 'y': 8}
+        square = Base.create(**square_dict)
+        self.assertEqual(square.id, 2)
+        self.assertEqual(square.size, 6)
+        self.assertEqual(square.x, 7)
+        self.assertEqual(square.y, 8)
 
-    #     # Test with a list of Square objects
-    #     list_squares = Base.load_from_file()
-    #     self.assertEqual(list_squares, [{'id': 3, 'size': 10, 'x': 2, 'y': 8}, {
-    #                      'id': 4, 'size': 2, 'x': 0, 'y': 0}])
+    def test_save_to_file(self):
+        """Test the save_to_file method of the Base class"""
+         # Test with a list of Rectangle objects
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles = [r1, r2]
+        Base.save_to_file(list_rectangles)
+        with open("Rectangle.json", "r") as file:
+        self.assertEqual(file.read(
+        ), '[{"y": 8, "x": 2, "id": 1, "width": 10, "height": 7}, {"y": 0, "x": 0, "id": 2, "width": 2, "height": 4}]')
 
-    # def test_to_json_string(self):
-    #     """Test the to_json_string method of the Base class"""
-    #     # Test with a list of Rectangle objects
-    #     r1 = Rectangle(10, 7, 2, 8)
-    #     r2 = Rectangle(2, 4)
-    #     list_rectangles = [r1, r2]
-    #     json_string = Base.to_json_string(list_rectangles)
-    #     expected_string = '[{"y": 8, "x": 2, "id": 1, "width": 10, "height": 7}, {"y": 0, "x": 0, "id": 2, "width": 2, "height": 4}]'
-    #     self.assertEqual(json_string, expected_string)
+         # Test with a list of Square objects
+        s1 = Square(10, 2, 8)
+        s2 = Square(2)
+        list_squares = [s1, s2]
+        Base.save_to_file(list_squares)
+        with open("Square.json", "r") as file:
+            self.assertEqual(file.read(
+            ), '[{"id": 3, "size": 10, "x": 2, "y": 8}, {"id": 4, "size": 2, "x": 0, "y": 0}]')
 
-    #     # Test with a list of Square objects
-    #     s1 = Square(10, 2, 8)
-    #     s2 = Square(2)
-    #     list_squares = [s1, s2]
-    #     json_string = Base.to_json_string(list_squares)
-    #     expected_string = '[{"id": 3, "size": 10, "x": 2, "y": 8}, {"id": 4, "size": 2, "x": 0, "y": 0}]'
-    #     self.assertEqual(json_string, expected_string)
+    def test_load_from_file(self):
+         """Test the load_from_file method of the Base class"""
+         # Test with a list of Rectangle objects
+         list_rectangles = Base.load_from_file()
+         self.assertEqual(list_rectangles, [{'y': 8, 'x': 2, 'id': 1, 'width': 10, 'height': 7}, {
+                          'y': 0, 'x': 0, 'id': 2, 'width': 2, 'height': 4}])
 
-    # def test_from_json_string(self):
-    #     """Test the from_json_string method of the Base class"""
-    #     # Test with a JSON string
-    #     json_string = '[{"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}, {"id": 2, "size": 6, "x": 7, "y": 8}]'
-    #     list_dicts = Base.from_json_string(json_string)
-    #     expected_list = [{'id': 1, 'width': 2, 'height': 3, 'x': 4, 'y': 5},
-    #                      {'id': 2, 'size': 6, 'x': 7, 'y': 8}]
-    #     self.assertEqual(list_dicts, expected_list)
+         # Test with a list of Square objects
+         list_squares = Base.load_from_file()
+         self.assertEqual(list_squares, [{'id': 3, 'size': 10, 'x': 2, 'y': 8}, {
+                          'id': 4, 'size': 2, 'x': 0, 'y': 0}])
 
-    #     # Test with an empty string
-    #     empty_string = ''
-    #     list_dicts = Base.from_json_string(empty_string)
-    #     expected_list = []
-    #     self.assertEqual(list_dicts, expected_list)
+    def test_to_json_string(self):
+         """Test the to_json_string method of the Base class"""
+         # Test with a list of Rectangle objects
+         r1 = Rectangle(10, 7, 2, 8)
+         r2 = Rectangle(2, 4)
+         list_rectangles = [r1, r2]
+         json_string = Base.to_json_string(list_rectangles)
+         expected_string = '[{"y": 8, "x": 2, "id": 1, "width": 10, "height": 7}, {"y": 0, "x": 0, "id": 2, "width": 2, "height": 4}]'
+         self.assertEqual(json_string, expected_string)
 
-    #     # Test with None
-    #     list_dicts = Base.from_json_string(None)
-    #     expected_list = []
-    #     self.assertEqual(list_dicts, expected_list)
+         # Test with a list of Square objects
+         s1 = Square(10, 2, 8)
+         s2 = Square(2)
+         list_squares = [s1, s2]
+         json_string = Base.to_json_string(list_squares)
+         expected_string = '[{"id": 3, "size": 10, "x": 2, "y": 8}, {"id": 4, "size": 2, "x": 0, "y": 0}]'
+         self.assertEqual(json_string, expected_string)
+
+    def test_from_json_string(self):
+         """Test the from_json_string method of the Base class"""
+         # Test with a JSON string
+         json_string = '[{"id": 1, "width": 2, "height": 3, "x": 4, "y": 5}, {"id": 2, "size": 6, "x": 7, "y": 8}]'
+         list_dicts = Base.from_json_string(json_string)
+         expected_list = [{'id': 1, 'width': 2, 'height': 3, 'x': 4, 'y': 5},
+                          {'id': 2, 'size': 6, 'x': 7, 'y': 8}]
+         self.assertEqual(list_dicts, expected_list)
+
+         # Test with an empty string
+         empty_string = ''
+         list_dicts = Base.from_json_string(empty_string)
+         expected_list = []
+         self.assertEqual(list_dicts, expected_list)
+
+         # Test with None
+         list_dicts = Base.from_json_string(None)
+         expected_list = []
+         self.assertEqual(list_dicts, expected_list)
 
 
 if __name__ == '__main__':
     unittest.main()
-    
