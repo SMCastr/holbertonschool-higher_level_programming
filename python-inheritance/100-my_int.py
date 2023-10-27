70ed27d0a346cb6f7e5b19e8c66321daac1fe95d
SMCastr <mariesweetycastro@gmail.com>
2023-10-27 13:53:15 -0400

    add file and code functions

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """ 100-main """
 
+class MyInt(int):
+    """ MyInt class"""
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
