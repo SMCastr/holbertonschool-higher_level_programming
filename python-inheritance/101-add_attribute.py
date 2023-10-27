ea7aca631878c303e47ffc769a4f33339bb34947
SMCastr <mariesweetycastro@gmail.com>
2023-10-27 11:20:41 -0400

    blank-spaces fix

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -9,4 +9,3 @@ def add_attribute(obj, attr_name, attr_value):
     if not hasattr(obj, '__dict__') and not (hasattr(obj, '__slots__') and attr_name in obj.__slots__):
         raise TypeError("can't add new attribute")
     setattr(obj, attr_name, attr_value)
-
