ab0eb8afbdd008c5aa81f326e802d299651ff990
SMCastr <mariesweetycastro@gmail.com>
2023-10-27 14:36:26 -0400

    lines structure

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -15,9 +15,9 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
+
     if last_name == "":
-        print(f"My name is {first_name}")
-    
+        print(f"My name is {first_name}")   
     elif first_name == "Bob"or last_name != "":
         print(f"My name is {first_name} {last_name}")
     else:
