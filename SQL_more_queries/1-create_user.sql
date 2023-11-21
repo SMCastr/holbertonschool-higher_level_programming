8a10f8ec131af99ca73e7f9ac7ff283ca008eb9e
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 18:50:27 -0400

    xx structure

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- 1-create_user.sql
--- Script to create the MySQL server user user_0d_1
+
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- Grant all privileges to 'user_0d_1' on all databases, tables, etc. (wildcard '*.*')
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
