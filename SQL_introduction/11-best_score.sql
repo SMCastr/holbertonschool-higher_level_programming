7593ea31bcfa27b747250784b0232ecfd2bbe7e6
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 17:01:39 -0400

    best of the best  of the best

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,3 @@
 -- 11-best_score.sql
 -- SQL query to list records with a score >= 10 in the second_table ordered by score (top first)
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
--- Exit from MySQL shell
-EXIT;
