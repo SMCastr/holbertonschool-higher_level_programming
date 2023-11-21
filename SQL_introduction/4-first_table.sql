9fac76e969e930abfecaa269e28279e232bedc1a
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 16:10:28 -0400

    create  first T

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- 4-first_table.sql
+-- SQL query to create the first_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+-- Exit from MySQL shell
+EXIT;
