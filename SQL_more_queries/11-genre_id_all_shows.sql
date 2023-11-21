c8d4198128fb2d15f72425cea2537388c4acac61
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 18:28:11 -0400

    list id  shw

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,12 @@
+-- 11-genre_id_all_shows.sql
+-- Script to list all shows from the 'hbtn_0d_tvshows' database, displaying title and genre_id
+
+-- Use the database 'hbtn_0d_tvshows'
+USE hbtn_0d_tvshows;
+
+-- Select shows and their corresponding genre IDs from the 'tv_shows' and 'tv_show_genres' tables
+-- If a show doesn't have a genre, display NULL
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
