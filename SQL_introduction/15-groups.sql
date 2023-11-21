cd9a8d12c6b77d5b3983266cf500bb2085feca56
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 16:39:02 -0400

    Grp. select

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- 15-groups.sql
+-- SQL query to list the number of records with the same score in the second_table
+SELECT score, COUNT(*) AS number FROM second_table
+GROUP BY score
+ORDER BY number DESC, score DESC;
+-- Exit from MySQL shell
+EXIT;
