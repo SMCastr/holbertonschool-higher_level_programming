cfb7acbfd13d7451398e9d454fe2122d69eb7e13
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 18:20:14 -0400

    empty never

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,12 @@
+-- 4-never_empty.sql
+-- Script to create the table id_not_null on the MySQL server
+
+-- Create or replace the table 'id_not_null' if it exists
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256),
+    PRIMARY KEY (id)
+);
+
+-- Optional: Display the structure of the 'id_not_null' table for verification
+DESCRIBE id_not_null;
