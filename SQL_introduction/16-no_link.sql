00aa140a9aad39a03feb8ad17e8cc2763b12bfb9
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 16:41:37 -0400

    lists records

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- 16-no_link.sql
+-- SQL query to list all records of the second_table with a name value
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
+-- Exit from MySQL shell
+EXIT;
+
