3d00842f4d54fea1b6c2fd467c7c5441650b451f
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 18:38:55 -0400

    modify structure

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,16 +2,11 @@
 -- Ensure that the users exist before checking their privileges
 -- Create user 'user_0d_1' if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
 -- Grant all privileges to 'user_0d_1' on all databases, tables, etc. (wildcard '*.*')
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
 -- Create user 'user_0d_2' if not exists
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Now, list the privileges for both users
 -- List privileges for 'user_0d_1'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
 -- List privileges for 'user_0d_2'
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
