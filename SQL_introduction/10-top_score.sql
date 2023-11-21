f9be6539d648434ab178165aacaf4c0a1e089604
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 16:27:01 -0400

    top top

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- 10-top_score.sql
+-- SQL query to list all records of the second_table ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
+-- Exit from MySQL shell
+EXIT;
