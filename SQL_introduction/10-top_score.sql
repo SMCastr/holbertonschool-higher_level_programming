97bc68357439d89bc372a185ecbb0a14ca3b4d9a
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 16:59:57 -0400

    fix spaces records

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,3 @@
 -- 10-top_score.sql
 -- SQL query to list all records of the second_table ordered by score (top first)
 SELECT score, name FROM second_table ORDER BY score DESC;
--- Exit from MySQL shell
-EXIT;
