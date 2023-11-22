f90b18f60475ff8f8df53d2ada92ffb6d1c5c53c
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 21:00:18 -0400

    order cities id

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- 9-cities_by_state_join.sql
+-- This script creates a table 'cities' in the database
 
--- Select all cities and their corresponding states using a JOIN
 SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
