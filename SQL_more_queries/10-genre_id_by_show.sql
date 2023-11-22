f0981204664dc5514d689cf9f6a456d9196aa055
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 22:24:26 -0400

    ids

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,10 +1,6 @@
 -- 10-genre_id_by_show.sql
 -- This script should list all shows and their genre IDs
 
--- Use the database 'hbtn_0d_tvshows'
-USE hbtn_0d_tvshows;
-
--- Select shows and genres
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
