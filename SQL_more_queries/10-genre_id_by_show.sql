b69417a8a3bd8b38ffdf5b9cc33a30cfdbbd111b
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 21:01:53 -0400

    title & genres

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
 -- 10-genre_id_by_show.sql
 -- Order by shows.title and genres.id
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
