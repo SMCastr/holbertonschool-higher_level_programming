9c21c59960d64294ad2aa6fc9e49c6b5bf44bf3e
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 22:39:51 -0400

    comedy list

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,13 +1,9 @@
 -- 15-comedy_only.sql
--- Script to list all Comedy shows in the 'hbtn_0d_tvshows' database
+-- List the titles of all comedy TV shows in alphabetical order.
 
--- Use the database 'hbtn_0d_tvshows'
-USE hbtn_0d_tvshows;
-
--- Select Comedy shows
-SELECT tv_shows.title
+ELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC
