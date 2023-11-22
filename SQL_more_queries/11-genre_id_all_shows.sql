3d227d209af0e8af8a37cf3f2688713209bbd275
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 22:26:49 -0400

    xxx grenres id

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,11 +1,7 @@
 -- 11-genre_id_all_shows.sql
 -- This script should list all shows, and all genres linked to that show
 
--- Use the database 'hbtn_0d_tvshows'
-USE hbtn_0d_tvshows;
-
--- If a show doesn't have a genre, display NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
