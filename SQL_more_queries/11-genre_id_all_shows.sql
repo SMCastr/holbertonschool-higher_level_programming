e31f388030ce72e0e181a369af60cc051a771c85
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 21:12:13 -0400

    select genreid

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,11 +1,6 @@
 -- 11-genre_id_all_shows.sql
--- Script to list all shows from the 'hbtn_0d_tvshows' database, displaying title and genre_id
+-- This script creates a table 'tv_shows' in the database
 
--- Use the database 'hbtn_0d_tvshows'
-USE hbtn_0d_tvshows;
-
--- Select shows and their corresponding genre IDs from the 'tv_shows' and 'tv_show_genres' tables
--- If a show doesn't have a genre, display NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
