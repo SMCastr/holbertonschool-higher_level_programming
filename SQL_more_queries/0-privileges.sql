da28fa77e9dce959e0a775811ec89d6f3acf137f
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 20:26:12 -0400

    fix  structure- privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,14 +1,8 @@
 -- 0-privileges.sql
--- Script to list all privileges of MySQL users on the localhost
--- Ensure that the users exist before checking their privileges
--- Create user 'user_0d_1' if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
--- Grant all privileges to 'user_0d_1' on all databases, tables, etc. (wildcard '*.*')
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- Create user 'user_0d_2' if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
--- Now, list the privileges for both users
+-- Script to list all privileges of the MySQL users, on your server (in localhost).
+
 -- List privileges for 'user_0d_1'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
 -- List privileges for 'user_0d_2'
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
