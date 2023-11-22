5c515ac2fd2fd77dbfd2c5ca33ae4f93bd2bd948
SMCastr <mariesweetycastro@gmail.com>
2023-11-21 22:44:13 -0400

    spaces

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,10 +1,7 @@
 -- 3-force_name.sql
--- This script creates a table 'force_name' in the database
+-- Script to create a table 'force_name' in the database
 
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL,
 );
-
--- Optional: Display the structure of the 'unique_id' table for verification
-DESCRIBE unique_id;
