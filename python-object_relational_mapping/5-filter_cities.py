b9c80ffb2e5827f01fc20a6ee4df498698e08bf2
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 19:46:08 -0400

    spaces line fix

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -11,24 +11,17 @@ import sys
 
 if __name__ == "__main__":
     # Connect to MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     # Create a cursor object to interact with the database
     cursor = db.cursor()
 
     # Execute SQL query to get cities of a specific state
-    query = """
-        SELECT cities.name FROM cities
-        INNER JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """
+    query = ("SELECT cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC")
 
     # Execute the query with state name as an argument
     cursor.execute(query, (sys.argv[4],))
@@ -37,7 +30,7 @@ if __name__ == "__main__":
     result = cursor.fetchone()
 
     # Display the result
-    if result and result[0]:
+    if result[0]:
         print(result[0])
     else:
         print("Not found")
