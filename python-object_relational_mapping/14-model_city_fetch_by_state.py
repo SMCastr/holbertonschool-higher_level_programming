eee3189fff68d46d4f3bd8940e532296b91a2d31
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 18:16:11 -0400

    fix import & session

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,39 @@
 #!/usr/bin/python3
-"""Script that prints all City objects from the database hbtn_0e_14_usa
+"""
+Start link class to table in database
 """
 
-import sys
 from model_city import Base, City
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import func
+import sys
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    """Prints all City objects from the database passed as argument."""
 
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Connecting to a MySQL database.
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+
+    # Creating a premade "Session" class.
     Session = sessionmaker(bind=engine)
-    session = Session()
 
-    cities = session.query(City, State).join(State).order_by(City.id).all()
+    # Creating an instance of Session class.
+    my_session = Session()
 
-    for city, state in cities:
+    # Printing City objects.
+    for city, state in my_session.query(City, State).\
+            filter(City.state_id == State.id).order_by(City.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    # Closing session.
+    my_session.close()
