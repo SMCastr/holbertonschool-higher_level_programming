995a79c76b84d2f1b2717934d43709cf4ffcc350
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 19:34:47 -0400

    add query

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute SQL query to get cities of a specific state
+    query = """
+        SELECT GROUP_CONCAT(name SEPARATOR ', ')
+        FROM cities
+        WHERE state_id = (SELECT id FROM states WHERE name = %s)
+        ORDER BY id
+    """
+
+    # Execute the query with state name as an argument
+    cursor.execute(query, (sys.argv[4],))
+
+    # Fetch the result
+    result = cursor.fetchone()
+
+    # Display the result
+    if result[0]:
+        print(result[0])
+    else:
+        print("Not found")
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
