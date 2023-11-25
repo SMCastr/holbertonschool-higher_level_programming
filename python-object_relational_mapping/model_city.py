c1f942a0335609f9f6db05a0391268dc426a7ff2
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 18:20:59 -0400

    black spaces fix

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """Start link class to table in database 
 """
+
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
+
 
 Base = declarative_base()
 
