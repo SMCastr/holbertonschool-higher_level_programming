30d927debee004478547aea2f8701b10bc1c9973
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 18:41:55 -0400

    structure function

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""This module deletes all State objects with a name containing the letter a
-from the database passed as an argument. 
+"""This module deletes all State objects with a name containing
+the letter a from the database passed as an argument. 
 """
 
 
@@ -11,9 +11,12 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
 
     Session = sessionmaker(bind=engine)
     session = Session()
