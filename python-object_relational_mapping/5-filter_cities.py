9dfba31744a291324217603c0d2096ec3d402a52
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 19:41:35 -0400

    script all localhost cities

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -4,23 +4,30 @@ Script that takes in the name of a state as an argument and lists
 all cities of that state, using the database hbtn_0e_4_usa.
 """
 
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     # Connect to MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
     # Create a cursor object to interact with the database
     cursor = db.cursor()
 
     # Execute SQL query to get cities of a specific state
     query = """
-        SELECT GROUP_CONCAT(name SEPARATOR ', ')
-        FROM cities
-        WHERE state_id = (SELECT id FROM states WHERE name = %s)
-        ORDER BY id
+        SELECT cities.name FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
     """
 
     # Execute the query with state name as an argument
@@ -30,7 +37,7 @@ if __name__ == "__main__":
     result = cursor.fetchone()
 
     # Display the result
-    if result[0]:
+    if result and result[0]:
         print(result[0])
     else:
         print("Not found")
