6c322f2913f43e1a9cd561fc00b571003a72f5e8
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 18:48:38 -0400

    fix code fucntions

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_to_delete = session.query(State).filter(State.name.like('%a%')):
+    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
     for state in states_to_delete:
         session.delete(state)
     session.commit()
