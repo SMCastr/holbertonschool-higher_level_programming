363fa0e66f75b361e04e46c661a153383716aaca
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 18:47:23 -0400

    fix line long

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module deletes all State objects with a name containing
-the letter a from the database passed as an argument. 
+the letter a from the database passed as an argument.
 """
 
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    states_to_delete = session.query(State).filter(State.name.like('%a%')):
     for state in states_to_delete:
         session.delete(state)
     session.commit()
