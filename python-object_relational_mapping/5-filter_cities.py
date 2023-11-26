dbbe7af70bd671c286517de3fc13b89c5326e402
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 23:06:52 -0400

    modify structure warn

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -6,6 +6,12 @@ Script that lists all cities from the database hbtn_0e_4_usa.
 import MySQLdb
 import sys
 
+def print_cities(cities):
+    """
+    Prints cities with proper formatting.
+    """
+    print(", ".join(city[1] for city in cities))
+
 if __name__ == "__main__":
     # Check if the correct number of arguments is provided
     if len(sys.argv) != 5:
@@ -13,8 +19,12 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # Connect to MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    except MySQLdb.Error as e:
+        print("Error connecting to the database:", e)
+        sys.exit(1)
 
     # Create a cursor object to interact with the database
     cursor = db.cursor()
@@ -29,16 +39,17 @@ if __name__ == "__main__":
     """
 
     # Execute the query with state name as an argument
-    cursor.execute(query, (sys.argv[4],))
+    try:
+        cursor.execute(query, (sys.argv[4],))
+        result = cursor.fetchall()
 
-    # Fetch the result
-    result = cursor.fetchall()
-
-    # Display the results
-    if result:
-        print(", ".join([row[1] for row in result]))
-    else:
-        print("Not found")
+        # Display the results
+        if result:
+            print_cities(result)
+        else:
+            print("Not found")
+    except MySQLdb.Error as e:
+        print("Error executing the query:", e)
 
     # Close cursor and database connection
     cursor.close()
