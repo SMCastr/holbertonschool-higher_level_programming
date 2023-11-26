c0023ca68b2cac4170328898035172ffe443026d
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 22:31:09 -0400

    blanck spaces

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,12 +11,13 @@ if __name__ == "__main__":
     # Connect to MySQL server running on localhost at port 3306
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    
+
     # Create a cursor object to interact with the database
     cursor = db.cursor()
 
     # Use format to create the SQL query with the user input
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(sys.argv[4])
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".
+    format(sys.argv[4])
 
     # Execute the query
     cursor.execute(query)
