d834fe96d7c242696b290243e7e02252c8c8905d
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 23:36:36 -0400

    add structure  table

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""
-City class definition
-"""
+"""City class definition"""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from relationship_state import Base, State
+from model_state import Base, State
 
 
 class City(Base):
-    """
-    City class
-    """
+    """City class"""
 
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-State class definition
-"""
+"""State class definition"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from relationship_city import Base, City
+from model_city import Base, City
 
 
 class State(Base):
-    """
-    State class
-    """
+    """State class"""
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
