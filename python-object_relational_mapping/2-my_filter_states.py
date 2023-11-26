017b49ca771e3e454dbbd6c477b9262ca77d04c4
SMCastr <mariesweetycastro@gmail.com>
2023-11-25 22:14:34 -0400

    filters create dbcourse

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to MySQL server running on localhost at port 3306
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Use format to create the SQL query with the user input
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(sys.argv[4])
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
